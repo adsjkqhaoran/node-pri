79e3cebf1356b605e25a974923248b355367e2e9
adsjkqhaoran <adsjkqhaoran@qq.com>
2017-06-30 11:42:41 +0800

    wechat demo

---
+++ koa/wechatDemo/app.js
@@ -0,0 +1,56 @@
+var Koa = require('koa');
+var WebSocket = require('ws');
+var router = require('koa-router')();
+var Cookies = require('cookies');
+var static = require('koa-static');
+var path = require('path');
+var bodyparse = require('koa-bodyparser');
+var {tmp} = require('./tmp.js');
+
+var app = new Koa();
+
+app.use(static(path.join(__dirname, 'public')));
+app.use(tmp());
+app.use(bodyparse());
+app.use(async(ctx, next) => {
+ 
+   await next();
+});
+router.get('/', async(ctx, next) => {
+    ctx.render('index.html');
+});
+router.post('/login', async(ctx, next) => {
+       var name = ctx.request.body.name;
+    //    http的header字符集支持US-ASCII子集的字符集[巨坑]
+       name = Buffer(name).toString('base64');
+       ctx.cookies.set('user',name);
+    //    name =Buffer(ctx.cookies.get('user'),'base64').toString();
+       ctx.render('wechat.html',{user:ctx.request.body.name});
+});
+
+app.use(router.routes());
+var server = app.listen(3000, function () {
+    console.log('[SERVER] listen in 3000');
+});
+var WebSocketServer = WebSocket.Server;
+var wss = new WebSocketServer({
+    server: server
+})
+//由于api改动req已经在第二个参数里了
+wss.on('connection',function(ws,req){
+    console.log('[SERVER] connecting');
+    if(req.headers){
+        var cookies = new Cookies(req,null);
+        var name = cookies.get('user');
+        name = Buffer(name,'base64').toString();
+    }
+    ws.on('message',function(msg){
+        console.log(msg);
+        wss.broadcast(`${name}: ${msg}`);
+    })
+})
+wss.broadcast = function (data) {
+    wss.clients.forEach(function (client) {
+        client.send(data);
+    });
+};
---
+++ koa/wechatDemo/tmp.js
@@ -0,0 +1,44 @@
+const nunjucks = require('nunjucks'); //模板引擎
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape && true,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+})
+var tmp = function () {
+    return async(ctx, next) => {
+        //给ctx添加render方法 
+        ctx.render = function (file, data) {
+            ctx.response.tyle = "text/html";
+            ctx.response.body = env.render(file, data);
+        }
+        //继续执行后面的中间件
+        await next();
+    }
+}
+
+exports.tmp = tmp;
