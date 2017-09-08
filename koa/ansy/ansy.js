c7c8afe196cebc1b55d3e8e0b7603f1bb8398429
haox <adsjkqhaoran>
2017-09-08 17:42:21 +0800

    emphasize the koa async demo

--- koa/ansy/ansy.js
+++ koa/ansy/ansy.js
@@ -9,14 +9,22 @@ app.use(async (ctx, next) => {
 
 app.use(async (ctx, next) => {
     const start = new Date().getTime(); // 当前时间
-    await next(); // 调用下一个middleware
+    let result = await next(); // 调用下一个middleware
+    console.log(result);
     const ms = new Date().getTime() - start; // 耗费时间
     console.log(`Time: ${ms}ms`); // 打印耗费时间
 });
 
 app.use(async (ctx, next) => {
-      ctx.response.type = 'text/html';
-      ctx.response.body = '<h1>Hello, koa2!</h1>';
+    //如果是异步的话必须要是promise 不然会立刻执行
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            ctx.response.type = 'text/html';
+            ctx.response.body = '<h1>Hello, koa2!</h1>';
+            resolve('ok');
+        },2000);
+    })
+
 });
 app.listen(8080);
 console.log('app 8080');
