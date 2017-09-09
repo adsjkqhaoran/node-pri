35125bb8fa29ae97d33ea0769ea5da193937a7a7
haox <adsjkqhaoran>
2017-09-09 17:27:32 +0800

    add message

--- koa/mocha/async.js
+++ koa/mocha/async.js
@@ -1,6 +1,7 @@
 var fs = require('mz/fs'); //把异步读取文件由callback改为promise的库
  //没有es7的话 需要自己调用done函数来结束
-// 参考 http://www.ruanyifeng.com/blog/2015/12/a-mocha-tutorial-of-examples.html
+ // 参考 http://www.ruanyifeng.com/blog/2015/12/a-mocha-tutorial-of-examples.html
+ // 测试http的话 可以配合.expect('Content-Type', /text\/html/)断言响应体 和 .expect(200, '<h1>Hello, Bob!</h1>')响应内容
 module.exports = async () =>{
    var file = await  fs.readFile('./test.txt','utf-8');
    var r = new Function('return ' + file);
